7d6903e8590f030c211f351d085c50f77840d12a
Hessia <1540496026@qq.com>
2024-03-13 17:15:40 +0800

    board information

--- contracts/Donate.sol
+++ contracts/Donate.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 contract Donate{
+    
     struct Donator{
-        string name;
-        uint256 amount;
-        address delegator;
         uint256 targetId;
+        string name;
+        address donator_addr;
+        uint256 amount;
     }
     struct Board{
         uint256 id;
@@ -19,16 +20,14 @@ contract Donate{
     
     
     uint256 public boardIndex;
-    mapping(address=>Donator) public donators;
+    Donator[] public donators;
     Board[] public boards;
     constructor(){
         boardIndex=0;
     }
     function createBoard(string calldata hostname,address  host,string calldata _name,string calldata _context,uint256  _targetAmount) public {
-        
         Board memory boardItem=Board(boardIndex,_name,_context,host,hostname,_targetAmount,0);
         boards.push(boardItem);
-
     }
     function getBoardInfo(uint256 _boardIndex) public view returns(Board memory)
     {
@@ -38,4 +37,5 @@ contract Donate{
     {
         return boards;
     }
+    
 }
